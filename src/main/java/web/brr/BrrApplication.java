1919b1141e2a9dc12ea85031d91c197b679f56d5
Maracujacake <madlordx@gmail.com>
2024-08-02 22:15:27 -0300

    Initial commit with index and HomeController

---
+++ src/main/java/web/brr/BrrApplication.java
@@ -0,0 +1,12 @@
+package web.brr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrrApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/brr/BrrApplicationTests.java
@@ -0,0 +1,13 @@
+package web.brr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
