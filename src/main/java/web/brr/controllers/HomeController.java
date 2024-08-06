1cb009a5233291fc96b66978e2be34867d6ea1da
Ivan_cn <ivan.capeli@catijr.com.br>
2024-08-06 18:14:34 -0300

    feat: Adicionei arquivos para internacionalizacao, organizei o projeto de acordo com o pedido pelo professor, adicionei o modulo de segurança(talvez falte pequenas coisas a serem configuradas nele).

--- src/main/java/web/brr/controllers/HomeController.java
+++ src/main/java/web/brr/controllers/HomeController.java
@@ -7,22 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import web.brr.domains.Cliente;
-import web.brr.repositories.ClienteRep;
-import web.brr.repositories.LocadoraRep;
+import web.brr.service.impl.UserService;
 
 @Controller
 public class HomeController {
 
     @Autowired
-    private ClienteRep clienteRep;
+    private UserService userService;
 
-    @Autowired
-    private LocadoraRep locadoraRep;
-
-    // @GetMapping("/")
-    // public String index() {
-    // return "index";
-    // }
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
     @GetMapping("/publicos/cadastro")
     public String showCadastro(Model model) {
@@ -30,20 +26,26 @@ public class HomeController {
         return "publicos/cadastroCliente";
     }
 
-    @PostMapping("/cadastro")
+    @PostMapping("/publicos/cadastrar")
     public String saveCliente(Cliente cliente) {
         System.out.println(cliente.getNome());
-        clienteRep.save(cliente);
+        cliente.setRole("CLIENTE");
+
+        userService.save(cliente);
         return "index";
     }
 
-    @GetMapping("/")
-    public String testBD(Model model) {
+    // Apenas para debug
+    // @GetMapping("/")
+    // public String testBD(Model model) {
+    // List<Cliente> liCli = clienteRep.findAll();
+    // for (Cliente cliente : liCli) {
+    // System.out.println("smt " + cliente.getNome());
+    // }
+    // model.addAttribute("clientes", clienteRep.findAll());
+    // model.addAttribute("loc", locadoraRep.findAll());
 
-        model.addAttribute("clientes", clienteRep.findAll());
-        model.addAttribute("loc", locadoraRep.findAll());
-
-        return "publicos/testeBD";
-    }
+    // return "publicos/testeBD";
+    // }
 
 }
---
+++ src/main/java/web/brr/security/UsuarioDetailsService.java
@@ -0,0 +1,27 @@
+package web.brr.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import web.brr.domains.User;
+import web.brr.repositories.UserRep;
+
+public class UsuarioDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRep dao;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User usuario = dao.findByNome(username).orElse(null);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new UsuarioDetails(usuario);
+    }
+}
