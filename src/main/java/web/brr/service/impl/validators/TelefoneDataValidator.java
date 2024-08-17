8ef8ed5ecac933e7f796658e9d1a23b2f47da890
Ivan_cn <ivan.capeli@catijr.com.br>
2024-08-17 11:51:51 -0300

    feat: validando formato de cpf, cnpj e telefone.

--- src/main/java/web/brr/service/impl/validators/TelefoneDataValidator.java
+++ src/main/java/web/brr/service/impl/validators/TelefoneDataValidator.java
@@ -2,43 +2,50 @@ package web.brr.service.impl.validators;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Component;
 
 import web.brr.service.spec.DataValidatorServiceSpec;
+
 @Component
 public class TelefoneDataValidator implements DataValidatorServiceSpec {
     @Override
     public boolean supports(Class<?> clazz) {
-        return String.class.isAssignableFrom(clazz);    
+        return String.class.isAssignableFrom(clazz);
     }
-      
+
     public boolean supports(Class<?> clazz, String attrName) {
-        if(attrName.equals("telefone")) {
+        if (attrName.equals("telefone")) {
             return supports(clazz);
         }
         return false;
     }
+
     @Override
     public List<String> validate(Object target) {
         List<String> errors = new ArrayList<>();
 
         String telefone = (String) target;
-        
+
         if (telefone == null || telefone.isEmpty()) {
             errors.add("Telefone é obrigatorio");
         }
+        String pattern = "^\\(\\d{2}\\) \\d{5}-\\d{4}$";
 
-        // TODO:  Não faz sentido verificar a formatação, apenas se é um número válido. Mas se precisar, fazer aqui.
+        if (!Pattern.matches(pattern, telefone)) {
+            errors.add("Telefone invalido");
+            return errors;
+        }
         telefone = telefone.replaceAll("[^0-9]", "");
 
         // xx x xxxx xxxx telefone pode ter 8, 9 ou 11 digitos
         int telLength = telefone.length();
-        if(telLength > 11 || telLength < 8 ||  telLength==10){
+        if (telLength > 11 || telLength < 8 || telLength == 10) {
             errors.add("Telefone invalido");
         }
 
         return errors;
     }
-    
+
 }
