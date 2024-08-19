683772f462b0de5c3cda959ec093485269f77d6a
VMila <vmila@LAPTOP-QSJ6BQ77>
2024-08-19 14:31:46 -0300

    traducao frase tela inicial

--- admin_tests.sql
+++ admin_tests.sql
@@ -1,9 +1,8 @@
 -- insercao de admin
 INSERT INTO user (id, nome, email, senha, role) VALUES 
-(10, 'Adao', 'Adao@peganomeupao.com', '123456', 'ROLE_ADMIN');
+(10, 'Adm', 'Admin@example.com', '123456', 'ROLE_ADMIN');
 
 INSERT INTO admin (id, privelegios) VALUES 
 (10, 'alguns ai');
 
--- senha: pedro 
 UPDATE user SET senha = '$2a$10$Mu1WbGZvpAsw.FRW4RLfcOsysF8PLNaO5vKDxoOadIiaFMtmADh4S' WHERE id = 10;
